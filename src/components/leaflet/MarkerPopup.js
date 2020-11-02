3ea7952dafb082a80fb06beb15739a4ed454767d
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-11-02 11:08:57 +0100

    like button

--- src/components/leaflet/MarkerPopup.js
+++ src/components/leaflet/MarkerPopup.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Popup} from 'react-leaflet';
+import request from "../../utils/request";
+import endpoints from "../../endpoints";
+import {useAuth0} from "@auth0/auth0-react";
 
 //this componentn open the popup whenver we click on a marker on the map.
 const MarkerPopup = (props) => {
   const { ...data } = props.data;
+  const[like, setLike] = useState([]);
 
+  let {
+    loading,
+    loginWithRedirect,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  //call the method getlikes by location and show amount of likes in popup
+  async function getLikes() {
+    let likes = await request(
+        `${process.env.REACT_APP_SERVER_URL}${endpoints.getLikesByLocation}/${data.id}`,
+        getAccessTokenSilently,
+        loginWithRedirect
+    );
+
+    if (likes && likes.length > 0) {
+      console.log(likes);
+      setLike(likes.length);
+    }
+  }
+  getLikes();
+
+  const body = JSON.stringify({
+        "FK_Location": data.id,
+        "isLiked": 1
+      }
+  )
+
+  const likeClickHandler = async (event) => {
+    /* Prevent the form submission from reloading the page */
+    event.preventDefault();
+
+    /* post method with a form, CF request.js */
+    let newLikeResponse = await request(
+        `${process.env.REACT_APP_SERVER_URL}${endpoints.postLike}`,
+        getAccessTokenSilently,
+        loginWithRedirect,
+        "POST",
+        body
+    )
+    //let newLocation =  newLocationResponse.toJSON();
+    console.log(newLikeResponse);
+   // setLike([...like,{
+    //  FK_Location:newLikeResponse.FK_Location,
+     // FK_User:newLikeResponse.FK_User,
+      //isLiked:newLikeResponse.isLiked
+    //}]);
+  };
 
   return  (<Popup>
     <h1 className='popup-text'>{data.name}</h1>
@@ -14,6 +65,10 @@ const MarkerPopup = (props) => {
     <p className='popup-text'>Closes at : {data.closingTime}</p>
     <p className='popup-text'>Category : {data.fK_Category}</p>
     <p className='popup-text'>City : {data.fK_City}</p>
+    <p className='popup-text'>Likes : {like}</p>
+
+    <button
+        onClick={likeClickHandler}>A button! </button>
   </Popup>);
 };
 
