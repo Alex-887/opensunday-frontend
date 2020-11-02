7bc795f2c7611d87cdb824dc8f320ad7bc5078c3
laurin <laurin.weber@students.hevs.ch>
2020-11-02 11:16:09 +0100

    Like button working

--- src/components/leaflet/MarkerPopup.js
+++ src/components/leaflet/MarkerPopup.js
@@ -6,70 +6,72 @@ import {useAuth0} from "@auth0/auth0-react";
 
 //this componentn open the popup whenver we click on a marker on the map.
 const MarkerPopup = (props) => {
-  const { ...data } = props.data;
-  const[like, setLike] = useState([]);
+    const {...data} = props.data;
+    const [likes, setLikes] = useState([]);
 
-  let {
-    loading,
-    loginWithRedirect,
-    getAccessTokenSilently,
-  } = useAuth0();
-
-  //call the method getlikes by location and show amount of likes in popup
-  async function getLikes() {
-    let likes = await request(
-        `${process.env.REACT_APP_SERVER_URL}${endpoints.getLikesByLocation}/${data.id}`,
-        getAccessTokenSilently,
-        loginWithRedirect
-    );
-
-    if (likes && likes.length > 0) {
-      console.log(likes);
-      setLike(likes.length);
-    }
-  }
-  getLikes();
-
-  const body = JSON.stringify({
-        "FK_Location": data.id,
-        "isLiked": 1
-      }
-  )
-
-  const likeClickHandler = async (event) => {
-    /* Prevent the form submission from reloading the page */
-    event.preventDefault();
-
-    /* post method with a form, CF request.js */
-    let newLikeResponse = await request(
-        `${process.env.REACT_APP_SERVER_URL}${endpoints.postLike}`,
-        getAccessTokenSilently,
+    let {
+        loading,
         loginWithRedirect,
-        "POST",
-        body
+        getAccessTokenSilently,
+    } = useAuth0();
+
+    //call the method getlikes by location and show amount of likes in popup
+    async function getLikes() {
+        let likes = await request(
+            `${process.env.REACT_APP_SERVER_URL}${endpoints.getLikesByLocation}/${data.id}`,
+            getAccessTokenSilently,
+            loginWithRedirect
+        );
+
+        if (likes && likes.length > 0) {
+            console.log(likes);
+            setLikes(likes);
+        }
+    }
+
+    getLikes();
+
+    const body = JSON.stringify({
+            "FK_Location": data.id,
+            "isLiked": 1
+        }
     )
-    //let newLocation =  newLocationResponse.toJSON();
-    console.log(newLikeResponse);
-   // setLike([...like,{
-    //  FK_Location:newLikeResponse.FK_Location,
-     // FK_User:newLikeResponse.FK_User,
-      //isLiked:newLikeResponse.isLiked
-    //}]);
-  };
 
-  return  (<Popup>
-    <h1 className='popup-text'>{data.name}</h1>
-    <h2 className='popup-text'>{data.address}</h2>
-    <p className='popup-text'>Telephone : {data.telephone}</p>
-    <p className='popup-text'>Opens at : {data.openingTime}</p>
-    <p className='popup-text'>Closes at : {data.closingTime}</p>
-    <p className='popup-text'>Category : {data.fK_Category}</p>
-    <p className='popup-text'>City : {data.fK_City}</p>
-    <p className='popup-text'>Likes : {like}</p>
+    const likeClickHandler = async (event) => {
+        /* Prevent the form submission from reloading the page */
+        event.preventDefault();
 
-    <button
-        onClick={likeClickHandler}>A button! </button>
-  </Popup>);
+        /* post method with a form, CF request.js */
+        let newLikeResponse = await request(
+            `${process.env.REACT_APP_SERVER_URL}${endpoints.postLike}`,
+            getAccessTokenSilently,
+            loginWithRedirect,
+            "POST",
+            body
+        )
+        //let newLocation =  newLocationResponse.toJSON();
+        console.log(newLikeResponse);
+        setLikes([...likes, {
+            FK_Location: newLikeResponse.FK_Location,
+            FK_User: newLikeResponse.FK_User,
+            isLiked: newLikeResponse.isLiked
+        }]);
+    };
+
+    return (<Popup>
+        <h1 className='popup-text'>{data.name}</h1>
+        <h2 className='popup-text'>{data.address}</h2>
+        <p className='popup-text'>Telephone : {data.telephone}</p>
+        <p className='popup-text'>Opens at : {data.openingTime}</p>
+        <p className='popup-text'>Closes at : {data.closingTime}</p>
+        <p className='popup-text'>Category : {data.fK_Category}</p>
+        <p className='popup-text'>City : {data.fK_City}</p>
+        <p className='popup-text'>Likes : {likes.length}</p>
+
+        <button
+            onClick={likeClickHandler}>A button!
+        </button>
+    </Popup>);
 };
 
 export default MarkerPopup;
