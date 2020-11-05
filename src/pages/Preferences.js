c58601794af09bbd7254e9388348b07802f989b8
laurin <laurin.weber@students.hevs.ch>
2020-11-05 15:47:19 +0100

    some comments

--- src/pages/Preferences.js
+++ src/pages/Preferences.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from "react";
-import MapView from "../components/leaflet/MapView";
 import request from "../utils/request";
 import endpoints from "../endpoints.json";
 import {useAuth0} from "@auth0/auth0-react";
@@ -7,12 +6,32 @@ import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import Preference from "./Preference";
 import ListGroup from "react-bootstrap/ListGroup";
 
-
 function Preferences() {
-
+    //Declaration Variables
+    //============== Hooks =================
     const [locations, setLocations] = useState([]);
 
+    let {
+        loginWithRedirect,
+        getAccessTokenSilently,
+    } = useAuth0();
 
+    useEffect(() => {
+        async function getLocations() {
+            let locations = await request(
+                `${process.env.REACT_APP_SERVER_URL}${endpoints.user}`,
+                getAccessTokenSilently,
+                loginWithRedirect
+            );
+            if (locations && locations.length > 0) {
+                console.log(locations);
+                setLocations(locations);
+            }
+        }
+        getLocations();
+    }, []);
+
+    //============== methods - functions =================
     let handleLocationsClick = async (e) => {
         e.preventDefault();
 
@@ -28,27 +47,6 @@ function Preferences() {
             }
         }
     };
-
-    useEffect(() => {
-        async function getLocations() {
-            let locations = await request(
-                `${process.env.REACT_APP_SERVER_URL}${endpoints.user}`,
-                getAccessTokenSilently,
-                loginWithRedirect
-            );
-            if (locations && locations.length > 0) {
-                console.log(locations);
-                setLocations(locations);
-            }
-        }
-
-        getLocations();
-    }, []);
-
-    let {
-        loginWithRedirect,
-        getAccessTokenSilently,
-    } = useAuth0();
 
     return (
         <div className="LocationsDetails">
